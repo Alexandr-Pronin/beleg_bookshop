5dbf326a0e831d93c925b063e8c73557c2714bc8
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2023-01-09 16:19:48 +0100

    no cors

--- src/store/store.js
+++ src/store/store.js
@@ -79,7 +79,7 @@ let store = new Vuex.Store({
                 state.cart.map(function (item) {
                     if (item.product.ProduktID === state.cart[index].product.ProduktID)
                         item.product.Lagerbestand--;
-                });
+                })
             }
         },
 
@@ -100,7 +100,7 @@ let store = new Vuex.Store({
     actions: {
         GET_PRODUCTS_FROM_API_PHP({ commit }) {
             console.log("GET_PRODUCTS_FROM_API_PHP Start");
-            return fetch("https://ivm108.informatik.htw-dresden.de/ewa/g10/praktikum_ewa/php/KatalogBeleg.php")
+            return fetch("https://iws107.informatik.htw-dresden.de/ewa/g11/PHP/beleg_fetch.php")
                 .then(response => response.json())
                 .then((products) => {
                     commit('SET_PRODUCTS_TO_STATE', products);

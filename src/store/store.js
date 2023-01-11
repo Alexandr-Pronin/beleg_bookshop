c353081a798f127c50cac7dc01a3df1a81f98b6d
Alexander Pronin <aleksandr.pronin@htw-dresden.de>
2023-01-11 18:39:07 +0100

    link geändert

--- src/store/store.js
+++ src/store/store.js
@@ -100,7 +100,7 @@ let store = new Vuex.Store({
     actions: {
         GET_PRODUCTS_FROM_API_PHP({ commit }) {
             console.log("GET_PRODUCTS_FROM_API_PHP Start");
-            return fetch("https://iws107.informatik.htw-dresden.de/ewa/g11/PHP/beleg_fetch.php")
+            return fetch("https://ivm108.informatik.htw-dresden.de/ewa/g10/praktikum_ewa/php/KatalogBeleg.php")
                 .then(response => response.json())
                 .then((products) => {
                     commit('SET_PRODUCTS_TO_STATE', products);
